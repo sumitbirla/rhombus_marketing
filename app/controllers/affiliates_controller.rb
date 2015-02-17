1c3b7d76f962973f9fc680520c3fc40157e1f385
Sumit Birla <sbirla@tampahost.net>
2015-02-17 21:59:27 +0100

    referral logic

---
+++ app/controllers/affiliates_controller.rb
@@ -0,0 +1,43 @@
+class AffiliatesController < ApplicationController
+  
+  # GET /aff/<id>?dest=xxx
+  def capture
+    aff = AffiliateCampaign.find(params[:id])
+    
+    unless aff.nil?
+      if cookies[:acid].blank? || cookies[:acid] != params[:id]
+        sql = "UPDATE mktg_affiliate_campaigns SET raw_clicks=raw_clicks+1 AND unique_clicks=unique_clicks+1 WHERE ID=#{params[:id]}"
+        cookies[:acid] = params[:id]
+      else
+        sql = "UPDATE mktg_affiliate_campaigns SET raw_clicks=raw_clicks+1 WHERE ID=#{params[:id]}"
+      end
+      ActiveRecord::Base.connection.execute(sql)
+    end
+    
+    return redirect_to params[:dest] unless params[:dest].blank?
+    
+    if aff && !aff.destination_url.blank?
+      redirect_to aff.destination_url
+    else
+      redirect_to root_path
+    end
+  end
+  
+  # GET /ref/<key>?dest=xxx
+  def referral
+    cookies[:ref_key] = { value: params[:key], expires: 30.days.from_now }
+    sql = "UPDATE core_user SET referral_clicks=referral_clicks+1 WHERE referral_key='#{params[:key]}'"
+    ActiveRecord::Base.connection.execute(sql)
+
+    unless params[:dest].blank?
+      redirect_to params[:dest]
+    else
+      redirect_to root_path
+    end
+  end
+  
+  # GET /fbref/<key>?ddid=xxx&dest=xxx
+  def facebook_referral
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   resources :email_subscribers
   
+  get "/aff/:id" => "affiliates#capture"
+  get "/ref/:key" => "affiliates#referral"
+  get "/fbref/:key" => "affiliates#facebook_referral"
   get "eb" => "email_blasts#redirect"
   get "pixel" => "email_blasts#pixel"
   get "ocr/:uuid" => "email_subscribers#one_click_remove"
