b02fc838c5c45bb4227629aa75984547913ffc41
Sumit Birla <sbirla@tampahost.net>
2015-04-21 21:19:44 -0400

    email bounce job update

--- app/jobs/email_bounce_job.rb
+++ app/jobs/email_bounce_job.rb
@@ -14,7 +14,8 @@ class EmailBounceJob < ActiveJob::Base
     @dry_run = false
     
     begin
-      Net::POP3.start('127.0.0.1', 110, 'deals', 'deals321user') do |pop|
+      Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
+      Net::POP3.start('pinto.tampahost.net', 995, 'deals', 'deals321user') do |pop|
 
         pop.each_mail do |mail|
           msg = Mail.new(mail.pop)
